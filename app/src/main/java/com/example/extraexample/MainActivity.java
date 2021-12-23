3d773b2e77aedea76253d518382f91b1ede18439
gykim0923 <gykim0923@gmail.com>
2021-12-23 11:27:49 +0900

    explicit intent 구현

--- app/src/main/java/com/example/extraexample/MainActivity.java
+++ app/src/main/java/com/example/extraexample/MainActivity.java
@@ -1,23 +1,49 @@
 package com.example.extraexample;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityResultLauncher<Intent> activityResultLauncher; // 런처 시작
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        activityResultLauncher = registerForActivityResult(//런처 생성
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        Intent intent = result.getData();
+                        String s= intent.getStringExtra("result");
+                        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+                        //안에서는 this가 아닌 applicationContext로 받음
+                    }
+                }
+        );
     }
 
     public void onClickButton(View v){ //버튼에 onclick 설정 넣어야함
         Intent intent = new Intent(this, NewActivity.class);
         intent.putExtra("str","Hello world");
         intent.putExtra("number",123);
-        startActivity(intent);
+//        startActivity(intent);
+        activityResultLauncher.launch(intent);
     }
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//    }
 }
--- app/src/main/java/com/example/extraexample/NewActivity.java
+++ app/src/main/java/com/example/extraexample/NewActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 public class NewActivity extends AppCompatActivity {
@@ -17,5 +18,16 @@ public class NewActivity extends AppCompatActivity {
         int b = intent.getIntExtra("number",-4);// putExtra와 다르게 get 타입 으로 해야함
         //default값이 있을 수 있음
         Toast.makeText(this, a+b, Toast.LENGTH_SHORT).show();
+
+
     }
+    public void onClickButton(View v){
+        Intent intent = new Intent();
+        intent.putExtra("result","Hello");
+        setResult(RESULT_OK,intent);
+
+        finish();// finish를 해야 종료 됐을 때 전달됨
+
+    }
+
 }
